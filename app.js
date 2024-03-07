9e9c8ad0d3ccc009745d8f24315cf9679fb5b2e9
kaikaci12 <somewonesomewone@gmail.com>
2024-03-07 15:33:59 +0400

    add the generating number logic according to the level of problems

--- app.js
+++ app.js
@@ -10,4 +10,14 @@ const levelPrompt = chooseLevel(prompt("Level: "));
 function generateRandomNumber(level) {
   let x;
   let y;
+  if (level == 1) {
+    x = Math.floor(Math.random() * 10);
+    y = Math.floor(Math.random() * 10);
+  } else if (level == 2) {
+    x = Math.floor(Math.random() * 100);
+    y = Math.floor(Math.random() * 100);
+  } else if (level == 3) {
+    x = Math.floor(Math.random() * 1000);
+    y = Math.floor(Math.random() * 1000);
+  }
 }
