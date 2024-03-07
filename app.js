86bcc8cc2a9b53d5b28f9b8695c2dec0d9e67da8
kaikaci12 <somewonesomewone@gmail.com>
2024-03-07 15:31:26 +0400

    set up chooseLevel function that asks user to enter level while the level input is valid

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+function chooseLevel(level) {
+  while (isNaN(level) || level > 3 || level < 0) {
+    level = Number(prompt("Level: "));
+  }
+  return level;
+}
+
 const levelPrompt = chooseLevel(prompt("Level: "));
